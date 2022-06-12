1f5952502538816e2457b53a33e9639becde3814
atkhssn <atk.hssn2017@gmail.com>
2022-06-13 01:23:14 +0600

    Learn about php constant

---
+++ constant.php
@@ -0,0 +1,13 @@
+<?php
+
+/* php constant*/
+
+define('NAME', "Atik Hassan");     // define('constant variable name', variable value)
+
+define('marks', 80);
+
+echo NAME;
+
+echo "<br>";
+
+echo marks;
