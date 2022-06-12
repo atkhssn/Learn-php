f1e332a015e0cb50f781f2bf5f0ed1cfa92b651f
atkhssn <atk.hssn2017@gmail.com>
2022-06-13 01:52:41 +0600

    Learn about php assignment operator

---
+++ assignment-operator.php
@@ -0,0 +1,31 @@
+<?php
+
+/* php assignment operator */
+
+$numberOne  =   10;     // Assignment operator (=)
+$numberTwo  =   5;
+
+$numberOne      +=      $numberTwo;        // Same as ($numberOne + $numberTwo)
+echo "The sum is: " . $numberOne;
+echo "<br>";
+
+$numberOne    -=    $numberTwo;        // Same as ($numberOne - $numberTwo)
+echo "The subtraction is: " . $numberOne;
+echo "<br>";
+
+$numberOne    *=     $numberTwo;        // Same as ($numberOne * $numberTwo)
+echo "The multiplication is: " . $numberOne;
+echo "<br>";
+
+$numberOne    /=     $numberTwo;        // Same as ($numberOne / $numberTwo)
+echo "The division is: " . $numberOne;
+echo "<br>";
+
+$numberOne    %=     $numberTwo;        // Same as ($numberOne % $numberTwo)
+echo "The modulus is: " . $numberOne;
+echo "<br>";
+
+$x  = 2;
+$x   **=    $numberTwo;        // Same as ($numberOne ** $numberTwo)
+echo "The exponentiation is: " . $x;
+echo "<br>";
