effb8b9c728c6ce4eecfe3507c30e67178c5551d
atkhssn <atk.hssn2017@gmail.com>
2022-06-15 11:08:10 +0600

    Learn about php comparison operator

---
+++ comparison-operator.php
@@ -0,0 +1,36 @@
+<?php
+
+/*      Comparison Operator     */
+
+$numberOne = 10;
+$numberTwo = 10;
+$numberThree = 15;
+$numberFour = 5;
+
+
+echo $numberOne     ==      $numberTwo;      //  Equals to ( == )
+echo "<br>";
+
+echo $numberOne     ===     $numberTwo;      //     Equals to with value and data-type ( === )
+echo "<br>";
+
+echo $numberOne     !=      $numberThree;      //    Not Equals to ( != )
+echo "<br>";
+
+echo $numberOne     <>      $numberThree;      //    Not Equals to ( <> )
+echo "<br>";
+
+echo $numberOne     >       $numberFour;      //  Greater than ( > )
+echo "<br>";
+
+echo $numberOne     >=       $numberFour;      //  Greater than or Equals to ( >= )
+echo "<br>";
+
+echo $numberOne     <       $numberThree;      //  Less than ( < )
+echo "<br>";
+
+echo $numberOne     <=      $numberThree;      //  Less than or Equals to ( <= )
+echo "<br>";
+
+echo $numberOne     <=>      $numberTwo;      //  Spaceship, If it returns less than value is true then it prints -1, or two values are equal then it prints 0, or  it returns greater than values is true then it prints 1( <=> )
+echo "<br>";
