ff455cd81a8d3740a1b99e1717ba906d03b98fd2
atkhssn <atk.hssn2017@gmail.com>
2022-06-16 23:14:57 +0600

    Learn about php if esle statement

---
+++ if-else-statement.php
@@ -0,0 +1,12 @@
+<?php
+
+/*  if else statement    */
+
+$name = "Atik Hassan";
+$gender = "male";
+
+if ($gender == "male") {
+    echo "Hello, Mr. " . $name . "!";
+} else {
+    echo "Hello, Miss. " . $name . "!";
+}
