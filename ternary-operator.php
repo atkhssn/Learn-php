9c48b7d97185052008de464cc843fbc99e5815e1
atkhssn <atk.hssn2017@gmail.com>
2022-06-17 00:39:26 +0600

    Learn about ternary operator

---
+++ ternary-operator.php
@@ -0,0 +1,19 @@
+<?php
+
+/*     Ternary Operator     */
+
+/*
+
+    (condition) ? true statement : false statement;
+
+*/
+
+$x = 15;
+$y = 20;
+
+($x > 10) ? $a = "Greater" : $a = "Smaller";
+echo $a;
+echo "<br>";
+
+$b = $y == 10 ? "Equal" : "Not equal";
+echo $b;
